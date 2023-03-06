7143ff13e6cca577283616b8ae54abb394a0bd72
Alston Shi <ashi3454@bths.edu>
2023-03-06 01:20:56 -0500

    Intial react development

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react";
+import React from 'react';
+import axios from 'axios';
+
+const apiUrl = 'https://alstons.pythonanywhere.com/';
 
 function App() {
+  // const [menu, setMenu] = useState([]);
+  // useEffect(async () => {
+  //   axios.get(apiUrl + 'food_menu/dict')
+  //       .then((data)=> {
+  //           console.log(data);
+  //           debugger
+  //       })
+  // }, []);
+
+  // return(
+  //   <div className="app">
+  //     app
+  //   </div>
+
+  // );
+
+  // Testing getting data back from other api server 
+  const [people, setPeople] = useState([]);
+  const [error, setError] = useState('');
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await axios.get('https://swapi.dev/api/people/');
+        setPeople(data.data?.results);
+      } catch (e) {
+        setError('Something went wrong');
+      }
+    };
+    fetchData();
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Application Test">
+      {error && <h3>{error}</h3>}
+      {people.length ? (
+        <div>
+          {people.map((person) => (
+            <h1>{person.name}</h1>
+          ))}
+        </div>
+      ) : (
+        <div>loading ....</div>
+      )}
     </div>
-  );
+  );  
 }
 
 export default App;

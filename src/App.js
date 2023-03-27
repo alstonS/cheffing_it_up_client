1050b323d8866934742b322c73258d9c495612e1
Alston Shi <ashi3454@bths.edu>
2023-03-27 05:09:25 -0400

    two different api calls to the flask server

--- src/App.js
+++ src/App.js
@@ -14,7 +14,9 @@ function App() {
   //       })
   // }, []);
   const [menu, setMenu] = useState([]);
-  const [showData, setShowData] = useState(true);
+  const [foodTypes, setFoodTypes] = useState([]);
+  const [showData, setShowData] = useState(false);
+  const [showFoodTypes, setShowFoodTypes] = useState(false);
 
   const fetchData = () => {
     fetch(apiUrl + "/food_menu/dict")
@@ -29,6 +31,20 @@ function App() {
     console.log("In Error");
   };
 
+  const fetchFoodTypes = () => {
+    fetch(apiUrl+ "/food_types/list")
+      .then((res) => res.json())
+      .then((foodTypes) => {
+        setFoodTypes(foodTypes);
+        setShowFoodTypes(true);
+        console.log("There was a response");
+        console.log(foodTypes);
+      })
+      .catch((error) => console.error(error));
+    console.log("In Error");
+  };
+
+
 
   console.log(menu)
   console.log("Hello")
@@ -36,19 +52,37 @@ function App() {
   return (
     <div className="Testing React">
       <button onClick={fetchData}>Show Information</button>
-      {(menu && menu.Data) ? (
-        Object.entries(menu.Data).map(([key, value]) => (
-          <div key={key}>
-            <h3>{value.name}</h3>
-            <p>Meal of Day: {value['meal of Day']}</p>
-            <p>Ingredients: {value.ingredients}</p>
-            <p>Calories: {value.calories}</p>
-            <p>Macronutrients: {value.Macronutrients}</p>
-            <p>Micronutrients: {value.Micronutrients}</p>
-          </div>
-        ))        
-      ) : (
-        <p>Loading...</p>
+      <button onClick={fetchFoodTypes}>Show Food Types</button>
+      {showData && (
+        <>
+          {menu && menu.Data ? (
+            Object.entries(menu.Data).map(([key, value]) => (
+              <div key={key}>
+                <h3>{value.name}</h3>
+                <p>Meal of Day: {value["meal of Day"]}</p>
+                <p>Ingredients: {value.ingredients}</p>
+                <p>Calories: {value.calories}</p>
+                <p>Macronutrients: {value.Macronutrients}</p>
+                <p>Micronutrients: {value.Micronutrients}</p>
+              </div>
+            ))
+          ) : (
+            <p>Loading...</p>
+          )}
+        </>
+      )}
+      {showFoodTypes && (
+        <>
+          {foodTypes && foodTypes.food_types_list ? (
+            foodTypes.food_types_list.map((foodType) => (
+              <div key={foodType}>
+                <h3>{foodType}</h3>
+              </div>
+            ))
+          ) : (
+            <p>Loading...</p>
+          )}
+        </>
       )}
     </div>
   );

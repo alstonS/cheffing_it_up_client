fe2387774df985bfcf7a309a76903ceb47996026
Rosio Angeles <r.angeles@modaadmins-MacBook-Pro.local>
2023-04-26 03:13:23 -0400

    changed AddRecipe.js so that data gets added in the correct format

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 
 import './AddRecipe.css'
@@ -6,13 +6,28 @@ import './AddRecipe.css'
 const apiUrl = 'https://cheffing-it-up.herokuapp.com/';
 
 export const AddRecipe = () => {
-    //const [data, setData] = useState();
+    const [foodTypes, setFoodTypes] = useState([]);
 
     const {
         register,
         handleSubmit,
         formState: { errors }
     } = useForm();
+
+    useEffect(() => {
+        const fetchFoodTypes = () => {
+            fetch(apiUrl + '/food_types/list')
+                .then((res) => res.json())
+                .then((foodTypes) => {
+                    setFoodTypes(foodTypes['food_types_list']);
+                })
+                .catch((error) => {
+                    console.error(error);
+                    console.log('In Error');
+                });
+        };
+        fetchFoodTypes();
+    }, []);
 
     const addData = (data) => {
         const requestOptions = {
@@ -21,51 +36,94 @@ export const AddRecipe = () => {
             body: JSON.stringify(data),
         };
 
-        fetch(apiUrl + "recipes/add", requestOptions)
-            .then((res) => res.json())
-            .catch((error) => console.error(error));
+        fetch(apiUrl + 'recipes/add', requestOptions)
+            .then((res) => {
+                res.json();
+                if (res.status === 200) {
+                    alert('Sucessfully added a recipe.');
+                } else {
+                    alert('Error adding a recipe.');
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+                alert('Error adding a recipe.');
+            });
     };
 
     const onSubmit = (data) => {
-
-        data.Macronutrients = JSON.parse(data.Macronutrients);
-        data.Micronutrients = JSON.parse(data.Micronutrients);
-        console.log(JSON.stringify(data));
+        let macros = {
+            'Protein': parseFloat(data['Protein']),
+            'Carbohydrates': parseFloat(data['Carbohydrates']),
+            'Fat': parseFloat(data['Fat'])
+        };
+        let micros = {
+            'Vitamin A': parseFloat(data['Vitamin A']),
+            'Vitamin C': parseFloat(data['Vitamin C']),
+            'Calcium': parseFloat(data['Calcium']),
+            'Iron': parseFloat(data['Iron'])
+        };
+        data['calories'] = parseFloat(data['calories']);
+        data.Macronutrients = macros;
+        data.Micronutrients = micros;
+        //making sure these fields won't appear in the database
+        delete data['Protein']
+        delete data['Carbohydrates']
+        delete data['Fat']
+        delete data['Fats']
+        delete data['Vitamin A']
+        delete data['Vitamin C']
+        delete data['Calcium']
+        delete data['Iron']
         addData(data);
     };
 
 
 
     return (
-        <div className="App">
+        <div className='AddRecipe'>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="form-control">
+                <div className='form-control'>
                     <label>Recipe Name</label>
-                    <input type="text" required name="name" {...register("name")} />
+                    <input type='text' required name='name' {...register('name')} />
                 </div>
-                <div className="form-control">
+                <div className='form-control'>
                     <label>Meal of Day</label>
-                    <input type="text" required name="meal of Day" placeholder='(breakfast, lunch, dinner)'{...register("meal of Day")} />
+                    <select {...register('meal of Day')}>
+
+                        {foodTypes?.map((currentFoodType, index) => {
+                            return <option key={index} >
+                                {currentFoodType}
+                            </option>
+                        })}
+                    </select>
                 </div>
-                <div className="form-control">
+                <div className='form-control'>
                     <label>Ingredients</label>
-                    <input type="text" required name="ingredients" placeholder='comma separated'{...register("ingredients")} />
+                    <input type='text' required name='ingredients' placeholder='comma separated'{...register('ingredients')} />
                 </div>
-                <div className="form-control">
+                <div className='form-control'>
                     <label>Calories</label>
-                    <input type="number" required name="calories" {...register("calories")} />
+                    <input type='number' required name='calories' {...register('calories')} />
                 </div>
-                <div className="form-control">
+                <div className='form-control'>
                     <label>Macronutrients</label>
-                    <input type="text" required name="Macronutrients" {...register("Macronutrients")} />
+                    <div> Protein: <input type='number' required name='Protein' {...register('Protein')} /> grams </div>
+                    <div> Carbohydrates: <input type='number' required name='Carbohydrates' {...register('Carbohydrates')} /> grams </div>
+                    <div> Fat: <input type='number' required name='Fat' {...register('Fat')} /> grams </div>
+
                 </div>
-                <div className="form-control">
+                <div className='form-control'>
                     <label>Micronutrients</label>
-                    <input type="text" required name="Micronutrients" {...register("Micronutrients")} />
+                    <div> Vitamin A: <input type='number' required name='Vitamin A' {...register('Vitamin A')} /> grams </div>
+                    <div> Vitamin C: <input type='number' required name='Vitamin C:' {...register('Vitamin C')} /> grams </div>
+                    <div> Calcium: <input type='number' required name='Calcium' {...register('Calcium')} /> grams </div>
+                    <div> Iron: <input type='number' required name='Iron' {...register('Iron')} /> grams </div>
+
                 </div>
-                <div className="form-control">
+                <div className='form-control'>
                     <label></label>
-                    <button type="submit">Add Recipe</button>
+                    <button type='submit'>Add Recipe</button>
                 </div>
             </form>
         </div>

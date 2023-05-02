f91798d2795011408d8826fb749a27c0abd4fd71
Alston Shi <ashi3454@bths.edu>
2023-05-02 02:58:59 -0400

    revert changes

--- src/components/FoodList.js
+++ src/components/FoodList.js
@@ -39,7 +39,7 @@ function FoodList() {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const res = await fetch(apiUrl + "recipes/dict");
+                const res = await fetch(apiUrl + "/recipes/dict");
                 const menu = await res.json();
                 setMenu(menu);
             } catch (error) {
